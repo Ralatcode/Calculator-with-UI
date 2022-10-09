646296a1f68fbc881f5a2a1465bff713a166edb2
RalatPC <ralatrado@gmail.com>
2022-10-10 00:05:30 +0100

    html button styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightGrey: '#D4D4D2',
+        EerieBlack: '#1C1C1C',
+        DarkLiver: '#505050',
+        VividGamboge: '#FF9500'
+      },
+    },
   },
   plugins: [],
 }
