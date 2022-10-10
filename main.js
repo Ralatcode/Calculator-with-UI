cc5070fc05b2de4a2deeb1ff24af263a725876c7
RalatPC <ralatrado@gmail.com>
2022-10-10 02:23:49 +0100

    function for displaying number pressed and storing display

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const resultScreen = document.querySelector('#screen');
+const numKeys = document.querySelectorAll('#num-keys');
+
+numKeys.forEach(numKey => {
+    numKey.addEventListener('click', clickNumber);
+});
+
+function clickNumber() {
+    if (resultScreen.textContent === '0') {
+        resultScreen.textContent = this.textContent;
+    } else {
+        resultScreen.textContent += this.textContent;
+    } 
+    
+    const displayValue = resultScreen.textContent;
+    return displayValue;
+}
 let num1;
 let num2;
 
