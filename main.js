efcc462f74db3394e6aa016ee6712a0a5824b5e3
RalatPC <ralatrado@gmail.com>
2022-10-10 04:13:26 +0100

    calculator works

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const resultScreen = document.querySelector('#screen');
 const numKeys = document.querySelectorAll('#num-keys');
 const operators = document.querySelectorAll('#operations');
+const equalBtn = document.querySelector('#equal-btn');
 
 numKeys.forEach(numKey => {
     numKey.addEventListener('click', clickNumber);
@@ -10,19 +11,45 @@ operators.forEach(operator => {
     operator.addEventListener('click', operateValues);
 });
 
+equalBtn.addEventListener('click', calculate);
+
+let num1 = '';
+let num2 = '';
+let currentOperator = '';
+
 function clickNumber() {
-    if (resultScreen.textContent === '0') {
-        resultScreen.textContent = this.textContent;
+    if (currentOperator === '') {
+        if (resultScreen.textContent === '0') {
+            resultScreen.textContent = this.textContent;
+        } else {
+            resultScreen.textContent += this.textContent;
+        }
     } else {
-        resultScreen.textContent += this.textContent;
-    } 
+        if (resultScreen.textContent === '0') {
+            resultScreen.textContent = this.textContent;
+            num2 = resultScreen.textContent;
+        } else {
+            resultScreen.textContent += this.textContent;
+            num2 = resultScreen.textContent;
+        }
+    }
 }
 
 function operateValues() {
-    console.log(this.textContent);
+    num1 = resultScreen.textContent;
+    currentOperator = this.textContent;
+    resultScreen.textContent = '0';
 }
-let num1;
-let num2;
+
+function calculate() {
+    const result = operate(currentOperator, num1, num2);
+    resultScreen.textContent = result;
+    console.log(currentOperator);
+    console.log(num1);
+    console.log(num2);
+    console.log(result);
+}
+
 
 function operate(operator, num1, num2) {
     if (operator === '+') {
